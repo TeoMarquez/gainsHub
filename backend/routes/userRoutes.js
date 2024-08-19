e1760065788b93fd410e8d4e4cb7e48f59c7fa7d
Mateo Marquez <127628801+TeoMarquez@users.noreply.github.com>
2024-08-19 04:22:33 -0300

    rutas para api
    final

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, verifyAccount } = require('../controllers/userController');
+const { register, login, verifyAccount,getAccount, updateUser } = require('../controllers/userController');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/verify-email', verifyAccount);
+router.get('/user-info/:userId', getAccount);
+router.put('/update-user/:userId', updateUser);
 
 
 
